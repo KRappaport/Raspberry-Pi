d03c2b2fde2207a87ffba506447cb3601709a504
Kobi Rappaport <ymr994@gmail.com>
2016-03-17 15:55:40 +0200

    v2 int Comparisons

--- Alarm/Alarm_Clock.c
+++ Alarm/Alarm_Clock.c
@@ -3,12 +3,17 @@
 
 int main()
 {
+
 	time_t test;
-	char c[] = "07:00";
-	while(1)
-	{
-		test = time(NULL);
-		if(strncmp(c,ctime(&test)+11,5)==0)
+        struct tm *info;
+        int wake_hour = 7;
+        int wake_minute = 0;
+
+        while(1)
+        {
+                test = time(NULL);
+                info = localtime(&test);
+                if(info->tm_hour==wake_hour && info->tm_min==wake_minute)
 		{
 			system("mpc play");
 			break;
