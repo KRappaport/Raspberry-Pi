8690ca651a6366c10a4aa18f41243b3464d522ed
Kobi Rappaport <ymr994@gmail.com>
2016-03-27 14:55:18 +0300

    Checks that mpc works

--- Alarm/Alarm_Clock.c
+++ Alarm/Alarm_Clock.c
@@ -7,8 +7,10 @@ int main()
 
 	time_t test;
         struct tm *info;
-        int wake_hour = 7;
-        int wake_minute = 0;
+        int wake_hour = 9;
+        int wake_minute = 50;
+	FILE *fp;
+        char c[100];
 
         while(1)
         {
@@ -16,7 +18,13 @@ int main()
                 info = localtime(&test);
                 if(info->tm_hour==wake_hour && info->tm_min==wake_minute)
 		{
-			system("mpc play");
+			do{
+				system("mpc stop");
+				sleep(1);
+				system("mpc play");
+				fp = fopen("/proc/asound/card0/pcm0p/sub0/status", "r");
+				fscanf(fp, "%s", c);
+			}while(strcmp(c, "closed") == 0);
 			break;
 		}
 		sleep(5);
