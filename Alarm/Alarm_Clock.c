bdc7b71687f09b643db784e12a75bfc73ec19118
root <root@kobi-ThinkPad-L540.(none)>
2016-04-05 21:06:24 +0300

    Minor Fixes

--- Alarm/Alarm_Clock.c
+++ Alarm/Alarm_Clock.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
 
 	time_t test;
-        struct tm *info;
-        int wake_hour = 7;
-        int wake_minute = 0;
+  struct tm *info;
+  int wake_hour = 7;
+  int wake_minute = 0;
 	FILE *fp;
-        char c[100];
+  char c[100];
 
-        while(1)
-        {
-                test = time(NULL);
-                info = localtime(&test);
-                if(info->tm_hour==wake_hour && info->tm_min==wake_minute)
+  while(1)
+  {
+    test = time(NULL);
+    info = localtime(&test);
+    if(info->tm_hour==wake_hour && info->tm_min==wake_minute)
 		{
-			do{
-				system("mpc stop");
-				sleep(1);
-				system("mpc play");
-				sleep(3);
-				fp = fopen("/proc/asound/card0/pcm0p/sub0/status", "r");
-				fscanf(fp, "%s", c);
-			}while(strcmp(c, "closed") == 0);
-			break;
+		do{
+			system("mpc stop");
+			sleep(1);
+			system("mpc play");
+			sleep(3);
+			fp = fopen("/proc/asound/card0/pcm0p/sub0/status", "r");
+			fscanf(fp, "%s", c);
+		}while(strcmp(c, "closed") == 0);
+		break;
 		}
 		sleep(5);
 	}
 	return(0);
 }
-
