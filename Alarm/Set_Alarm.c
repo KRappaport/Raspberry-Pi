aff4a284611592229f8f96861f8b33b6a530d873
KRappaport <ymr994@gmail.com>
2016-04-12 10:16:26 +0300

    Now sets if alarm should repeat

--- Alarm/Set_Alarm.c
+++ Alarm/Set_Alarm.c
@@ -5,21 +5,27 @@ int main(int argc, char const *argv[])
 
     FILE *hour;
     FILE *minute;
-    int hr, min;
+    FILE *repeat;
+    int hr, min, rpt;
 
     hour = fopen("hour.txt", "w");
     minute = fopen("minute.txt", "w");
+    repeat = fopen("repeat.txt", "w");
 
     printf("Enter alarm time: ");
     scanf("%d", &hr);
     scanf("%d", &min);
-    printf("ok\n");
+    printf("Choose if alarm should repeat (1 = repeat, 0 = don't repeat): ", );
+    scanf("%d", &rpt);
+    printf("OK alarm is set.\n");
 
     fprintf(hour, "%d", hr);
     fprintf(minute, "%d", min);
+    fprintf(repeat, "%d", rpt);
 
     fclose(hour);
     fclose(minute);
+    fclose(repeat);
 
     return(0);
 }
