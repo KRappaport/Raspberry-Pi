dc995802ced4bf94947e50f08ab7093aaa67b31a
KRappaport <ymr994@gmail.com>
2016-04-19 13:31:59 +0300

    Radio now started by its own function

--- Sleep.c
+++ Sleep.c
@@ -6,12 +6,12 @@
 #define MINUTE 60
 #define HOUR 3600
 
+void mpd_play(void);
+
 int main(int argc, char const *argv[]) {
 
     int hour, minute;
     long unsigned int sleep_time;
-    FILE *sound;
-    char sound_check[100];
 
     printf("Input sleep time: ");
     scanf("%d", &hour);
@@ -19,19 +19,28 @@ int main(int argc, char const *argv[]) {
 
     sleep_time = (hour*HOUR) + (minute*MINUTE);
 
-    do{
-        system("mpc stop");
-        sleep(1);
-        system("mpc play");
-        sleep(6);
-        sound = fopen("/proc/asound/card0/pcm0p/sub0/status", "r");
-        fscanf(sound, "%s", sound_check);
-        fclose(sound);
-    }while(strcmp(sound_check, "closed") == 0);
+    mpd_play();
 
     sleep(sleep_time);
 
     system("mpc stop");
 
     return(0);
+}
+
+void mpd_play(void){
+
+	FILE *sound;
+	char sound_check[100];
+
+	do{
+		system("mpc stop");
+		sleep(1);
+		system("mpc play");
+		sleep(6);
+		sound = fopen("/proc/asound/card0/pcm0p/sub0/status", "r");
+		fscanf(sound, "%s", sound_check);
+		fclose(sound);
+	}while(strcmp(sound_check, "closed") == 0);
+
 }
