de433e4e330cc59229ce47494931543f77067b9d
KRappaport <ymr994@gmail.com>
2017-03-15 21:03:47 +0200

    Many Chnages

--- Alarm/Alarm_Clock.c
+++ Alarm/Alarm_Clock.c
@@ -4,42 +4,78 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* typedef struct alarm_profile
+#define MIN_SEC 60
+#define HOUR_SEC 3600
+#define DAY_SEC 86400
+
+typedef struct alarm_profile
 {
 	int name_length;
-	char alarm_name[50];
-	short on_off;
-	short hour;
-	short minute;
-}ALARM */
+	char alarm_name[15];
+	short int on_off;
+	short int alarm_time[2];
+	short int volume;
+	short int rpt;
+	int priority;
+}ALARM;
 
+ALARM read_profile(char *path);
 void mpd_play(void);
 
 int main()
 {
 	time_t test;
 	struct tm *info;
-	short int on_off, alarm_time[2], rpt;
-	FILE *alarm_profile;
+	ALARM profile;
+	FILE *profile_list;
+	char path[55], vol_change[14];
+	int current_psec, alarm_psec;
+
+	profile_list = fopen("/home/pi/Raspberry-Pi/Alarm/profile_list.txt", "r");
+	fscanf(profile_list, "%s", path);
+
+	profile = read_profile(path);
+	alarm_psec = ((profile.alarm_time[0])*HOUR_SEC) + (profile.alarm_time[1]*MIN_SEC);
 
 	while(1)
 	{
-		alarm_profile = fopen("/home/pi/Raspberry-Pi/Alarm/Profiles/wake_up.alrm", "rb");
-		fread(&on_off, sizeof(short), 1, alarm_profile);
-		fread(alarm_time, sizeof(short), 2, alarm_profile);
-		fread(&rpt, sizeof(short), 1, alarm_profile);
-		fclose(alarm_profile);
 		test = time(NULL);
 		info = localtime(&test);
-		if(info->tm_hour==alarm_time[0] && info->tm_min==alarm_time[1])
+		current_psec = (info->tm_hour * HOUR_SEC) + (info->tm_min * MIN_SEC);
+		if(alarm_psec == current_psec)
 		{
+			sprintf(vol_change, "mpc volume %d", profile.volume);
+	        system(vol_change);
 			mpd_play();
 			if(!rpt)
 				break;
 		}
-		sleep(5);
+		else if (alarm_psec < current_psec) {
+			sleep(((DAY_SEC + alarm_psec) - current_psec)/2);
+		}
+		else {
+			sleep((alarm_psec - current_psec)/2);
+		}
 	}
 	return(0);
+}
+
+ALARM read_profile(char *path){
+	ALARM profile;
+	FILE *fp;
+
+	fp = fopen(path, "rb");
+
+	fread(&profile.name_length, sizeof(int), 1, fp);
+	fread(profile.alarm_name, sizeof(char), profile.name_length, fp);
+	fread(&profile.on_off, sizeof(short), 1, fp);
+	fread(profile.alarm_time, sizeof(short), 2, fp);
+	fread(&profile.volume, sizeof(short), 1, fp);
+	fread(&profile.rpt, sizeof(short), 1, fp);
+
+	profile.priority = ((int)profile.alarm_time[0] * 100) + (int)profile.alarm_time[1];
+
+	return(profile);
 }
 
 void mpd_play(void){
